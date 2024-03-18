8b56f55d50e233cbd8fad2e7ec0d8510d437ed19
“Akbermet <akbermetkuvatbekova05@gmail.com>
2024-03-18 18:21:06 -0500

    changes

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket                  = "terraform-s3-state-0223"
+    bucket                  = "terraform-s3-ak"
     key                     = "my-terraform-project"
-    region                  = "us-east-1"
+    region                  = "us-east-2"
     shared_credentials_file = "~/.aws/credentials"
   }
 }
