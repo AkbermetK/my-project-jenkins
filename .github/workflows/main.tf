d0906582ba71b455ccb496503cad3d75fcd9395d
“Akbermet <akbermetkuvatbekova05@gmail.com>
2024-03-18 17:52:32 -0500

    added them

---
+++ .github/workflows/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket                  = "terraform-s3-state-0223"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+}
+
+provider "aws" {
+  region   = "us-east-2"
+}
+
+resource "aws_ecr_repository" "my_ecr" {
+    name = "bar"
+    image_tag_mutability = "IMMUTABLE"
+
+    image_scanning_configuration {
+      scan_on_push = true
+    }
+}
